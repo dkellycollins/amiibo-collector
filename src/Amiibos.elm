22e754765f614ac0e33c373b2b0ce6290535156d
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-07-07 08:36:17 -0500

    Added the ability to pull amiibo data from the server.

--- src/Amiibos.elm
+++ src/Amiibos.elm
@@ -1,24 +1,49 @@
 module Amiibos exposing (..)
 
 import Http
-import Json.Decode
+import Json.Decode exposing (..)
+
 
 type alias AmiiboSeries =
-  { name: String
-  , displayName: String
-  }
+    { name : String
+    , displayName : String
+    }
+
 
 type alias Amiibo =
-  { name: String
-  , displayName: String
-  , releaseDate: Maybe String
-  , series: Maybe AmiiboSeries
-  }
+    { name : String
+    , displayName : String
+    , releaseDate : Maybe String
+    , series : Maybe AmiiboSeries
+    }
 
-type alias AmiiboList = List Amiibo
 
-amiiboSeriesDecoder: Json.Decode AmiiboSeries
+type alias AmiiboList =
+    List Amiibo
 
-amiiboDecoder: Json.Decode Amiibo
 
-amiiboListDecoder: Json.Decode AmiiboList
+getAmiiboList : (Result Http.Error AmiiboList -> msg) -> Cmd msg
+getAmiiboList msg =
+    let
+        url =
+            "https://amiibos-api.herokuapp.com/amiibos"
+
+        request =
+            Http.get url decodeAmiiboList
+    in
+    Http.send msg request
+
+
+decodeAmiiboList : Decoder AmiiboList
+decodeAmiiboList =
+    list decodeAmiibo
+
+
+decodeAmiibo : Decoder Amiibo
+decodeAmiibo =
+    map4 Amiibo (field "name" string) (field "displayName" string) (field "releaseDate" (nullable string)) (field "series" (nullable decodeAmiiboSeries))
+
+
+decodeAmiiboSeries : Decoder AmiiboSeries
+decodeAmiiboSeries =
+    map2 AmiiboSeries (field "name" string) (field "displayName" string)
--- src/App.elm
+++ src/App.elm
@@ -1,88 +1,103 @@
+module Main exposing (..)
+
+import Amiibos exposing (..)
 import Html
+import Http
 import Material
 import Material.Table
 import Maybe
 
-main = view getAmiibos
 
--- MODELS
+main : Program Never AmiiboList Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
-type alias AmiiboSeries =
-  { name: String
-  , displayName: String
-  }
 
-type alias Amiibo =
-  { name: String
-  , displayName: String
-  , releaseDate: Maybe String
-  , series: Maybe AmiiboSeries
-  }
 
-type alias AmiiboList = List Amiibo
+-- MESSAGES
+
+
+type Msg
+    = UpdatedAmiibos (Result Http.Error AmiiboList)
+
+
+
+-- INIT
+
+
+init : ( AmiiboList, Cmd Msg )
+init =
+    ( [], getAmiiboList UpdatedAmiibos )
+
+
 
 -- UPDATE
 
-type Msg
-  = UpdateModel
-  | New AmiiboList
 
-init: (AmiiboList, Cmd Msg)
-init =
-  ([], UpdateModel)
-
-update: Msg -> AmiiboList -> (AmiiboList, Cmd Msg)
+update : Msg -> AmiiboList -> ( AmiiboList, Cmd Msg )
 update msg model =
-  case msg of
-    UpdateModel ->
-      (getAmiibos, New)
-    New m -> 
-      (m, Cmd.none)
+    case msg of
+        UpdatedAmiibos (Result.Ok newList) ->
+            ( newList, Cmd.none )
+
+        UpdatedAmiibos (Result.Err _) ->
+            ( [], Cmd.none )
+
+
 
 -- VIEW
 
-view: List Amiibo -> Html.Html msg
-view amiibos = 
-  Material.Table.table [] 
-    [ Material.Table.thead []
-      [ Material.Table.th [] [(Html.text "Name")]
-      , Material.Table.th [] [(Html.text "Series")]
-      , Material.Table.th [] [(Html.text "Release Date")]      
-      ]
-    , Material.Table.tbody [] (List.map mapAmiiboToTableRow amiibos)
-    ]
 
-mapAmiiboToTableRow: Amiibo -> Html.Html msg
+view : List Amiibo -> Html.Html msg
+view amiibos =
+    Material.Table.table []
+        [ Material.Table.thead []
+            [ Material.Table.th [] [ Html.text "Name" ]
+            , Material.Table.th [] [ Html.text "Series" ]
+            , Material.Table.th [] [ Html.text "Release Date" ]
+            ]
+        , Material.Table.tbody [] (List.map mapAmiiboToTableRow amiibos)
+        ]
+
+
+mapAmiiboToTableRow : Amiibo -> Html.Html msg
 mapAmiiboToTableRow amiibo =
-  Html.tr []
-    [ Material.Table.td [] [(Html.text amiibo.displayName)]
-    , Material.Table.td [] [(Html.text (getAmiiboSeriesView amiibo.series))]    
-    , Material.Table.td [] [(Html.text (getReleaseDateView amiibo.releaseDate))]
-    ]
+    Html.tr []
+        [ Material.Table.td [] [ Html.text amiibo.displayName ]
+        , Material.Table.td [] [ Html.text (getAmiiboSeriesView amiibo.series) ]
+        , Material.Table.td [] [ Html.text (getReleaseDateView amiibo.releaseDate) ]
+        ]
 
-getReleaseDateView: Maybe String -> String
+
+getReleaseDateView : Maybe String -> String
 getReleaseDateView releaseDate =
-  Maybe.withDefault "N/A" releaseDate
+    Maybe.withDefault "N/A" releaseDate
 
-getAmiiboSeriesView: Maybe AmiiboSeries -> String
+
+getAmiiboSeriesView : Maybe AmiiboSeries -> String
 getAmiiboSeriesView amiiboSeries =
-  case amiiboSeries of
-    Nothing -> 
-      "N/A"
-    Just amiiboSeries ->
-      amiiboSeries.displayName
+    case amiiboSeries of
+        Nothing ->
+            "N/A"
 
-mapAmiiboToListItem: Amiibo -> Html.Html msg
+        Just amiiboSeries ->
+            amiiboSeries.displayName
+
+
+mapAmiiboToListItem : Amiibo -> Html.Html msg
 mapAmiiboToListItem amiibo =
-  Html.li [] [(Html.text (toString amiibo))]
+    Html.li [] [ Html.text (toString amiibo) ]
 
--- HTTP
 
-getAmiibos: AmiiboList
-getAmiibos =
-  [ Amiibo "mario_0" "Mario" (Just "2014-11-21") (Just (AmiiboSeries "super_smash_bros" "Super Smash Bros"))
-  , Amiibo "mario_1" "Mario" (Just "2015-03-20") (Just (AmiiboSeries "super_mario" "Super Mario"))
-  , Amiibo "donkey_kong_0" "Donkey Kong" (Just "2014-11-21") (Just (AmiiboSeries "super_smash_bros" "Super Smash Bros"))
-  , Amiibo "donkey_kong_1" "Donkey Kong" (Just "2016-11-04") (Just (AmiiboSeries "super_mario" "Super Mario"))
-  , Amiibo "fox" "Fox" (Just "2014-11-21") (Just (AmiiboSeries "super_smash_bros" "Super Smash Bros"))
-  ]
+
+--SUBSCRIPTIONS
+
+
+subscriptions : AmiiboList -> Sub Msg
+subscriptions model =
+    Sub.none
