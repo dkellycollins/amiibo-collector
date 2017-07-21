98ee88a55b342f2e3091e9e0d5c0834e96b0b8c0
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-07-20 22:08:00 -0500

    Updates to support running the proxy from the root directory.

--- proxy/index.js
+++ proxy/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var proxy = require('express-http-proxy');
+var path = require('path');
 
 var API_URL = process.env.API_URL;
 var PORT = process.env.PORT || 3000;
+var APP_PATH = path.join(__dirname, '../app')
 
 var app = express();
-app.use('/', express.static('../app'))
+app.use('/', express.static(APP_PATH))
 app.use('/api', proxy(API_URL));
 
 app.listen(PORT);
