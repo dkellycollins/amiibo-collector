92484174ad3b921b0ecff5c9e2ce78966aba3002
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-08-30 16:07:16 -0500

    Added amiibo images to the assets folder.

--- proxy.js
+++ proxy.js
@@ -5,10 +5,12 @@ var path = require('path');
 var API_URL = process.env.API_URL;
 var PORT = process.env.PORT || 3000;
 var APP_PATH = './';
+var ASSETS_PATH = './assets';
 
 var app = express();
 app.use('/', express.static(APP_PATH))
 app.use('/api', proxy(API_URL));
+app.use('/assets', express.static(ASSETS_PATH));
 
 app.listen(PORT);
 console.log('Proxy started on port [' + PORT + ']');
