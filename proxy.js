50246799913d1fcad9328a2b935e73d756bd0da8
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-09-15 14:26:48 -0500

    Cleanup

--- proxy.js
+++ proxy.js
@@ -2,10 +2,10 @@ var express = require('express');
 var proxy = require('express-http-proxy');
 var path = require('path');
 
-var API_URL = process.env.API_URL || 'https://amiibos-api.herokuapp.com';
-var PORT = process.env.PORT || 3000;
 var APP_PATH = './';
+var API_URL = process.env.API_URL || 'https://amiibos-api.herokuapp.com';
 var MEDIA_URL = process.env.MEDIA_URL || 'https://media.githubusercontent.com/media/dkellycollins/amiibo-collector/master/assets';
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 app.use('/', express.static(APP_PATH))
