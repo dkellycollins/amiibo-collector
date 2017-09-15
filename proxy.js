8923d0033b94fff8d0cc64aa57aa4471553ffeef
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-09-15 15:22:50 -0500

    Moved app code into www folder.

--- proxy.js
+++ proxy.js
@@ -2,13 +2,13 @@ var express = require('express');
 var proxy = require('express-http-proxy');
 var path = require('path');
 
-var APP_PATH = './';
+var APP_PATH = './www';
 var API_URL = process.env.API_URL || 'https://amiibos-api.herokuapp.com';
-var MEDIA_URL = process.env.MEDIA_URL || 'https://media.githubusercontent.com/media/dkellycollins/amiibo-collector/master/assets';
+var MEDIA_URL = process.env.MEDIA_URL || 'https://storage.googleapis.com/amiibo-collector';
 var PORT = process.env.PORT || 3000;
 
 var app = express();
-app.use('/', express.static(APP_PATH))
+app.use('/', express.static(APP_PATH));
 app.use('/api', proxy(API_URL));
 app.use('/assets', proxy(MEDIA_URL));
 
