291ff262116ff404619eaf6d3ef97b5976319d38
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-09-15 14:22:11 -0500

    Proxy assets url to github's storage.

--- proxy.js
+++ proxy.js
@@ -2,15 +2,15 @@ var express = require('express');
 var proxy = require('express-http-proxy');
 var path = require('path');
 
-var API_URL = process.env.API_URL;
+var API_URL = process.env.API_URL || 'https://amiibos-api.herokuapp.com';
 var PORT = process.env.PORT || 3000;
 var APP_PATH = './';
-var ASSETS_PATH = './assets';
+var MEDIA_URL = process.env.MEDIA_URL || 'https://media.githubusercontent.com/media/dkellycollins/amiibo-collector/master/assets';
 
 var app = express();
 app.use('/', express.static(APP_PATH))
 app.use('/api', proxy(API_URL));
-app.use('/assets', express.static(ASSETS_PATH));
+app.use('/assets', proxy(MEDIA_URL));
 
 app.listen(PORT);
 console.log('Proxy started on port [' + PORT + ']');
