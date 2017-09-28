b7b9af49b7564ff285027346d6ab3c93798550e7
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-09-28 14:17:09 -0500

    Added images to AmiibosTable.

--- src/AmiibosTable/View.elm
+++ src/AmiibosTable/View.elm
@@ -4,6 +4,7 @@ import Amiibos.Types
 import Date
 import Date.Extra
 import Html
+import Html.Attributes
 import Material.Options
 import Material.Table
 
@@ -12,7 +13,8 @@ view : Amiibos.Types.Amiibos -> Html.Html m
 view amiibos =
     Material.Table.table [ Material.Options.css "width" "100%" ]
         [ Material.Table.thead []
-            [ Material.Table.th [] [ Html.text "Name" ]
+            [ Material.Table.th [] []
+            , Material.Table.th [] [ Html.text "Name" ]
             , Material.Table.th [] [ Html.text "Series" ]
             , Material.Table.th [] [ Html.text "Release Date" ]
             ]
@@ -23,10 +25,21 @@ view amiibos =
 viewAmiiboTableRow : Int -> Amiibos.Types.Amiibo -> Html.Html m
 viewAmiiboTableRow index amiibo =
     Material.Table.tr []
-        [ Material.Table.td [] [ Html.text amiibo.displayName ]
+        [ Material.Table.td [] [ viewImage amiibo ]
+        , Material.Table.td [] [ Html.text amiibo.displayName ]
         , Material.Table.td [] [ viewAmiiboSeries amiibo.series ]
         , Material.Table.td [] [ viewReleaseDate amiibo.releaseDate ]
         ]
+
+
+viewImage : Amiibos.Types.Amiibo -> Html.Html msg
+viewImage amiibo =
+    Html.img
+        [ Html.Attributes.src ("https://storage.googleapis.com/amiibo-collector/" ++ amiibo.name ++ ".png")
+        , Html.Attributes.height 32
+        , Html.Attributes.width 32
+        ]
+        []
 
 
 viewReleaseDate : Maybe Date.Date -> Html.Html m
