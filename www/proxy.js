5097149bc2d2966087f4bf2f4889a5ee77b0cddb
Devin Kelly-Collins <devin.kellycollins@artisantechgroup.com>
2017-12-01 07:49:18 -0600

    Moved proxy script to www folder.

--- www/proxy.js
+++ www/proxy.js
@@ -2,7 +2,7 @@ var express = require('express');
 var proxy = require('express-http-proxy');
 var path = require('path');
 
-var APP_PATH = './www';
+var APP_PATH = `${__dirname}`;
 var API_URL = process.env.API_URL || 'https://amiibos-api.herokuapp.com';
 //var MEDIA_URL = process.env.MEDIA_URL || 'https://storage.googleapis.com/amiibo-collector';
 var PORT = process.env.PORT || 3000;
